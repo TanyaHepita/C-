2bd5d76666384a1129f66f420daf24f4e6051944
tanya haliche <tanya.haliche@epita.fr>
2022-09-25 21:00:17 +0200

    v2

---
+++ is_prime_prog/main.cpp
@@ -0,0 +1,35 @@
+//
+// Created by tanya on 25/09/22.
+//
+
+#include<iostream>
+
+
+int main(){
+    int n;
+    //std::string prime = "True";
+
+    while(std::cin >> n)
+    {
+        prime="True";
+        if (n <= 1)
+        {
+            prime = "False";
+            std::cout << n << " is a Prime : " << prime << '\n' ;
+            continue;
+        }
+        for (int i = 2; i < n/2; i++)
+            if (n % i == 0) {
+                prime = "False";
+                break;
+            }
+        std::cout << n << " is a Prime : " << prime << '\n';
+
+
+
+    }
+    return 0;
+
+
+
+}
