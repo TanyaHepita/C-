93058b06a8c4169ad7410613668956a09f0800fa
tanya haliche <tanya.haliche@epita.fr>
2022-09-28 14:24:17 +0200

    exercice1_final

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -10,21 +10,17 @@
 
 
 
-using std::cout; using std::cerr;
-using std::endl; using std::string;
-using std::ifstream; using std::vector;
-
 int main(int argc , char* argv[]) {
     if(argc != 0) {
         std::ifstream fileStream(argv[1]);
         if (fileStream.fail()) {
             //file could not be opened
-            std::cout << "The file " << argv[1] << "could not be opened." << std::endl;
+            std::cout << "The file " << argv[1] << " could not be opened." << std::endl;
             return 1;
         }
         int count = 0 ;
-        vector<string> words;
-        string word;
+        std::vector<std::string> words;
+        std::string word;
         while (fileStream >> word) {
             words.push_back(word);
             if (word.find(argv[2]) != std::string::npos)
