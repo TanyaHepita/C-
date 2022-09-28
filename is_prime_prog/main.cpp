611c98a1c5e69534e0a2e7a9a54c104db12950de
tanya haliche <tanya.haliche@epita.fr>
2022-09-28 14:25:16 +0200

    exercice1_final

--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -1,15 +1,11 @@
-//
-// Created by tanya on 25/09/22.
-//
-
 #include<iostream>
-
-
+#include<cmath>
 
 
 int main(){
     int n;
     std::string prime = "True";
+
 
     while(std::cin >> n)
     {
@@ -20,18 +16,14 @@ int main(){
             std::cout << n << " is a Prime : " << prime << '\n' ;
             continue;
         }
-        for (int i = 2; i < n/2; i++)
+        for (int i = 2; i < sqrt(n); i++)
             if (n % i == 0) {
                 prime = "False";
                 break;
             }
         std::cout << n << " is a Prime : " << prime << '\n';
 
-
-
     }
     return 0;
-
-
 
 }
