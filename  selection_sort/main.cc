7d98f20627b4a57ee8b6c2902cc03630b49cb062
TanyaHepita <99323309+TanyaHepita@users.noreply.github.com>
2022-11-23 22:40:30 +0100

    Delete  selection_sort directory

---  selection_sort/main.cc
+++
@@ -1,3 +0,0 @@
-//
-// Created by tanya on 23/11/22.
-//
---  selection_sort/my_sort.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by tanya on 23/11/22.
-//
-
-#ifndef MYFIRSTPROG_MY_SORT_HPP
-#define MYFIRSTPROG_MY_SORT_HPP
-
-#endif //MYFIRSTPROG_MY_SORT_HPP
